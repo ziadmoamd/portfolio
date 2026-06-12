a6884fd89e548a1e0cba1f12251fa095a47900e9
Ziad Mohamed Abrahim <ziadabrahimamir@gmail.com>
2026-06-13 00:09:20 +0200

    vite config

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Menu, X } from "lucide-react"
-import { AnimatePresence } from "framer-motion"
+import {motion , AnimatePresence } from "framer-motion"
 
 function Nav() {
     const [isOpen, setIsOpen] = useState(false)
